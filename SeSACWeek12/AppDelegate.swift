c1bae824257df9aef60bdb27e99f4735c389ef51
AlexHwang <cksgh0316@gmail.com>
2022-01-03 16:45:43 +0900

    -

--- SeSACWeek12/AppDelegate.swift
+++ SeSACWeek12/AppDelegate.swift
@@ -11,7 +11,7 @@ import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
- 
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print(#function)
         // Override point for customization after application launch.
@@ -21,20 +21,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // 알림 등록 (권한)
         if #available(iOS 10.0, *) {
-          // For iOS 10 display notification (sent via APNS)
-          UNUserNotificationCenter.current().delegate = self
-
-          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-          UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: { _, _ in }
-          )
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: { _, _ in }
+            )
         } else {
-          let settings: UIUserNotificationSettings =
+            let settings: UIUserNotificationSettings =
             UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-          application.registerUserNotificationSettings(settings)
+            application.registerUserNotificationSettings(settings)
         }
-
+        
         application.registerForRemoteNotifications()
         
         // 메세지 대리자 설정
@@ -42,41 +42,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // 현재 등록 토큰 가져오기
         Messaging.messaging().token { token, error in
-          if let error = error {
-            print("Error fetching FCM registration token: \(error)")
-          } else if let token = token {
-            print("FCM registration token: \(token)")
-          }
+            if let error = error {
+                print("Error fetching FCM registration token: \(error)")
+            } else if let token = token {
+                print("FCM registration token: \(token)")
+            }
         }
         
         
         
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         print(#function)
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         print(#function)
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
     // 포그라운드 수신: willPresent(로컬/푸시 동일)
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         
-        completionHandler([.list, .badge, .banner, .sound])
+        guard let rootViewController = (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.window?.rootViewController else { return }
+        
+        // 특정 신에서 푸시를 제외하고 싶을떄
+        if rootViewController is DetailViewController {
+            completionHandler([])
+        } else {
+            completionHandler([.list, .badge, .banner, .sound])
+        }
     }
     
     // 사용자가 로컬/푸시를 클릭했을 때 Response 호출 메서드
@@ -92,27 +99,71 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
             print("Other pushes")
         }
         
+        // SceneDelegate의 Window 객체 가져오기
+        guard let rootViewController = (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.window?.rootViewController else { return }
+        print(rootViewController)
+        
+        // 푸시 클릭시 화면 전환
+//        if rootViewController is SnapDetailViewController {
+//            rootViewController.present(DetailViewController(), animated: true, completion: nil)
+//        }
+        
+        if rootViewController is DetailViewController {
+//            let nav = UINavigationController(rootViewController: SnapDetailViewController())
+//            nav.modalPresentationStyle = .fullScreen
+//            rootViewController.navigationController?.present(nav, animated: true, completion: nil)
+            
+            rootViewController.navigationController?.pushViewController(SnapDetailViewController(), animated: true)
+            
+            rootViewController.dismiss(animated: true, completion: nil)
+        }
         
         completionHandler()
     }
     
     func application(application: UIApplication,
                      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-      Messaging.messaging().apnsToken = deviceToken
+        Messaging.messaging().apnsToken = deviceToken
     }
 }
 
 extension AppDelegate: MessagingDelegate {
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-      print("Firebase registration token: \(String(describing: fcmToken))")
-
-      let dataDict: [String: String] = ["token": fcmToken ?? ""]
-      NotificationCenter.default.post(
-        name: Notification.Name("FCMToken"),
-        object: nil,
-        userInfo: dataDict
-      )
-      // TODO: If necessary send token to application server.
-      // Note: This callback is fired at each app startup and whenever a new token is generated.
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
     }
+}
+
+extension UIViewController {
+    
+    var topViewController: UIViewController? {
+        return self.topViewController(currentViewController: self)
+    }
+    
+    // currentViewController: TabBarController 최상단에 떠있는 뷰 컨트롤러 도출
+    func topViewController(currentViewController: UIViewController) -> UIViewController {
+        if let tabBarController = currentViewController as? UITabBarController,
+           let selectedViewController = tabBarController.selectedViewController {
+            
+            return self.topViewController(currentViewController: selectedViewController)
+        } else if let navigationController = currentViewController as? UINavigationController,
+                  let visibleViewController = navigationController.visibleViewController {
+            
+            return self.topViewController(currentViewController: visibleViewController)
+        } else if let presentedViewController = currentViewController.presentedViewController {
+            return self.topViewController(currentViewController: presentedViewController)
+            
+        } else {
+            return currentViewController
+        }
+    }
+    
 }
